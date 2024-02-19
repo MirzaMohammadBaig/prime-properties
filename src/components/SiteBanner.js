d6c95b5431f4ee347f8280999c03b867c3712448
Mirza <mirzabaig.n321@gmail.com>
2024-02-19 23:45:22 +0530

    update path of link tag in sitebanner and image name

--- src/components/SiteBanner.js
+++ src/components/SiteBanner.js
@@ -11,10 +11,10 @@ function SiteBanner(){
                 <p className="mt-3 text-base text-gray-500 md:text-xl md:mt-5 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto lg:mx-0">At prime properties, we ensure that our clients get the best properties at affordable prices. Luxury is our priority and budget is our constrain. Pick and choose from 1000+ properties across the globe.</p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:flex lg:justify-start">
                     <div className="md:m-1">
-                        <Link to="" className="flex mb-2 font-semibold justify-center rounded-md bg-green-500 text-white px-8 py-4 hover:bg-green-600 transition duration-300">View properties</Link>
+                        <Link to="/prime-properties/" className="flex mb-2 font-semibold justify-center rounded-md bg-green-500 text-white px-8 py-4 hover:bg-green-600 transition duration-300">View properties</Link>
                     </div>
                     <div className="md:m-1">
-                        <Link to="" className="flex mb-2 font-semibold justify-center rounded-md bg-green-200 text-green-700 px-8 py-4 hover:bg-green-300 transition duration-300">Explore Locations</Link>
+                        <Link to="/prime-properties/" className="flex mb-2 font-semibold justify-center rounded-md bg-green-200 text-green-700 px-8 py-4 hover:bg-green-300 transition duration-300">Explore Locations</Link>
                     </div>
                 </div>
             </div>
